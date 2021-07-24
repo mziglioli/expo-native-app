ef85a5a2da0bc900d2b4eba32542784a366c9408
zigliolim <marceloziglioli@rentalcars.com>
2021-07-24 11:44:59 +0100

    feat(library): use latest library version

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,3 @@
+
+G
+hooks/useColorScheme.ts,0/f/0fb1f6509538b264e1500e0451f62b1d51c9afdb
--- App.tsx
+++ App.tsx
@@ -1,11 +1,20 @@
 import 'react-native-gesture-handler';
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 import useCachedResources from './hooks/useCachedResources';
 import useColorScheme from './hooks/useColorScheme';
-import Navigation from './navigation';
+import {
+  Provider as PaperProvider,
+} from 'react-native-paper';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+
+import { SideBar, Page, TabBottom, buildItems, tabScenes, tabRoutes } from '@mziglioli/react-native-components';
+import { View, Button } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+
+const Drawer = createDrawerNavigator();
+const SideBarItems = buildItems();
 
 export default function App() {
   const isLoadingComplete = useCachedResources();
@@ -15,10 +24,57 @@ export default function App() {
     return null;
   } else {
     return (
-      <SafeAreaProvider>
-        <Navigation colorScheme={colorScheme} />
-        <StatusBar />
-      </SafeAreaProvider>
+        <PaperProvider>
+          <SafeAreaProvider>
+            <NavigationContainer>
+              <Drawer.Navigator
+                  initialRouteName="Home"
+                  drawerContent={(props) => (
+                      <SideBar
+                          title="Some title"
+                          navigation={props.navigation}
+                          items={SideBarItems}
+                      />
+                  )}
+              >
+                <Drawer.Screen
+                    name="Home"
+                    component={(props) => (
+                        <View
+                            style={{
+                              flex: 1,
+                              flexDirection: 'column',
+                              backgroundColor: '#fff',
+                            }}
+                        >
+                          <Page page={'Home'} title={'Home'} navigation={props.navigation} />
+                          <TabBottom tabRoutes={tabRoutes} scenes={tabScenes} />
+                        </View>
+                    )}
+                />
+                <Drawer.Screen name="second">
+                  {(props) => (
+                      <Page
+                          page={'second'}
+                          title="second page"
+                          navigation={props.navigation}
+                      >
+                        <View>
+                          <Button
+                              onPress={() => {
+                                props.navigation.navigate('first');
+                              }}
+                              title={'second page'}
+                          />
+                        </View>
+                      </Page>
+                  )}
+                </Drawer.Screen>
+              </Drawer.Navigator>
+            </NavigationContainer>
+          </SafeAreaProvider>
+        </PaperProvider>
+
     );
   }
 }
